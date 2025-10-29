285a887db1b4cdb129cab447f179e68193adfe35
charan14111411 <charana@vayublue.com>
2025-10-29 16:58:12 +0530

    token expire date token lifespan increasing

--- app/utils/jwt_helper.py
+++ app/utils/jwt_helper.py
@@ -6,7 +6,7 @@ from flask import current_app
 def encode_token(user_id: int, hours: int = 1) -> str:
     payload = {
         "user_id": user_id,
-        "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=hours),
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(days=3650),
     }
     token = jwt.encode(payload, current_app.config["SECRET_KEY"], algorithm="HS256")
     return token
